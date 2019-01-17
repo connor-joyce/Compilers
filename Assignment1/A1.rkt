56c193c4d4663c9a9e4f1c723718f9ace3defba8
Connor <crjoyce2451@gmail.com>
2019-01-17 11:51:28 -0700

    Finished compiler, 35 tests pass. evalstr function added

--- Assignment1/A1.rkt
+++ Assignment1/A1.rkt
@@ -1,4 +1,7 @@
 #lang racket
+
+(require test-engine/racket-tests)
+
 ; a structure to hold a digit, this is used by the lexer
 ; a token type will be 'op, 'lparen, 'rparen, or 'digit or 'eof
 ; repr is a character representation
@@ -35,22 +38,22 @@
 (define (get-next-token input-port)
   (let ([input (read-char input-port)])
     (cond
-      [(equal? input #\+)     (token 'op #\+)]
-      [(equal? input #\*)     (token 'op #\*)]
-      [(equal? input #\()     (token 'lparen #\()]
-      [(equal? input #\))     (token 'lparen #\))]
-      [(equal? input #\0)     (token 'digit #\0)]
-      [(equal? input #\1)     (token 'digit #\1)]
-      [(equal? input #\2)     (token 'digit #\2)]
-      [(equal? input #\3)     (token 'digit #\3)]
-      [(equal? input #\4)     (token 'digit #\4)]
-      [(equal? input #\5)     (token 'digit #\5)]
-      [(equal? input #\6)     (token 'digit #\6)]
-      [(equal? input #\7)     (token 'digit #\7)]
-      [(equal? input #\8)     (token 'digit #\8)]
-      [(equal? input #\9)     (token 'digit #\9)]
-      [(equal? input eof)     (token 'eof eof)]
-      ;[else ]
+      [(equal? input #\+)     (token 'op input)]
+      [(equal? input #\*)     (token 'op input)]
+      [(equal? input #\()     (token 'lparen input)]
+      [(equal? input #\))     (token 'rparen input)]
+      [(equal? input #\0)     (token 'digit input)]
+      [(equal? input #\1)     (token 'digit input)]
+      [(equal? input #\2)     (token 'digit input)]
+      [(equal? input #\3)     (token 'digit input)]
+      [(equal? input #\4)     (token 'digit input)]
+      [(equal? input #\5)     (token 'digit input)]
+      [(equal? input #\6)     (token 'digit input)]
+      [(equal? input #\7)     (token 'digit input)]
+      [(equal? input #\8)     (token 'digit input)]
+      [(equal? input #\9)     (token 'digit input)]
+      [(equal? input eof)     (token 'eof input)]
+      [else (error "Invalid input: " input)]
       )))
       
 
@@ -64,4 +67,95 @@
 ; (() -> token) -> (ast-node | ast-expr-node)
 ; the parser takes a function (probably produced by lexstr) that
 ; lexes the contents of the input stream
-;(define (parser lex) ... )
+(define (parser lex)
+  (let* ([tok (lex)]
+         [rep (token-repr tok)]
+         [typ (token-type tok)])
+    (cond
+      [(eq? typ 'eof)     '()]
+      [(eq? typ 'digit)   (ast-node (string->number (string rep)))]
+      [(eq? typ 'lparen)  (parse-expr lex)])))
+
+(define (parse-expr lex)
+  (let* ([exp1 (parser lex)]
+         [op (parse-op lex)]
+         [exp2 (parser lex)]
+         [rparen? (lex)]
+         [rparen-type (token-type rparen?)])
+    (if (eq? rparen-type 'rparen)
+        (ast-expr-node op exp1 exp2)
+        (error "Invalid expression: " rparen-type))))
+
+(define (parse-op lex)
+  (let* ([tok (lex)]
+         [typ (token-type tok)]
+         [rep (token-repr tok)])
+    (if (eq? typ 'op)
+        rep
+        (error "Invalid operation: " rep))))
+
+(define (evaluate ast)
+  (cond
+    [(ast-node? ast)           (ast-node-val ast)]
+    [(ast-expr-node? ast)
+     (if (eq? #\+ (ast-expr-node-operator ast))
+         (+ (evaluate (ast-expr-node-left-child ast)) (evaluate (ast-expr-node-right-child ast)))
+         (* (evaluate (ast-expr-node-left-child ast)) (evaluate (ast-expr-node-right-child ast))))]))
+
+(define (evalstr str)
+  (evaluate (parser (lexstr str))))
+
+
+
+(check-expect (is-token-type? 'op) #t)
+(check-expect (is-token-type? 'lparen) #t)
+(check-expect (is-token-type? 'rparen) #t)
+(check-expect (is-token-type? 'digit) #t)
+(check-expect (is-token-type? 'foo) #f)
+(check-expect (is-token-type? 'eof) #t)
+
+; error cases on creating tokens
+(check-error (token 'op 1))
+(check-error (token 'foo "hello"))
+
+; test cases for get-next-token, note this implies get-next-token requires an input-port
+; as its sole argument...(that's a hint)
+(check-expect (get-next-token (open-input-string "0")) (token 'digit #\0))
+(check-expect (get-next-token (open-input-string "1")) (token 'digit #\1))
+(check-expect (get-next-token (open-input-string "2")) (token 'digit #\2))
+(check-expect (get-next-token (open-input-string "3")) (token 'digit #\3))
+(check-expect (get-next-token (open-input-string "4")) (token 'digit #\4))
+(check-expect (get-next-token (open-input-string "5")) (token 'digit #\5))
+(check-expect (get-next-token (open-input-string "6")) (token 'digit #\6))
+(check-expect (get-next-token (open-input-string "7")) (token 'digit #\7))
+(check-expect (get-next-token (open-input-string "8")) (token 'digit #\8))
+(check-expect (get-next-token (open-input-string "9")) (token 'digit #\9))
+(check-expect (get-next-token (open-input-string "+")) (token 'op #\+))
+(check-expect (get-next-token (open-input-string "*")) (token 'op #\*))
+(check-expect (get-next-token (open-input-string ")")) (token 'rparen #\)))
+(check-expect (get-next-token (open-input-string "(")) (token 'lparen #\())
+(check-expect (get-next-token (open-input-string "")) (token 'eof eof))
+(check-error (get-next-token (open-input-string "a")))
+
+; this should produce an error, it's not correct according to the grammar
+(check-error (parser (lexstr "(5)")))
+; but the rest of these are fine
+(check-expect (parser (lexstr "5")) (ast-node 5))
+(check-expect (parser (lexstr "(5+2)")) (ast-expr-node #\+ (ast-node 5) (ast-node 2)))
+(check-expect (parser (lexstr "(5+(3+2))")) (ast-expr-node #\+ (ast-node 5) (ast-expr-node #\+ (ast-node 3) (ast-node 2))))
+(check-expect (parser (lexstr "((5+3)+2)")) (ast-expr-node #\+ (ast-expr-node #\+ (ast-node 5) (ast-node 3)) (ast-node 2)))
+(check-expect (parser (lexstr "((5+3)+(2*3))")) (ast-expr-node #\+ (ast-expr-node #\+ (ast-node 5) (ast-node 3)) (ast-expr-node #\* (ast-node 2) (ast-node 3))))
+
+(check-expect (evalstr "5") 5)
+(check-expect (evalstr "(5+2)") 7)
+(check-expect (evalstr "(5+(3+2))") 10) 
+(check-expect (evalstr "((5+3)+2)") 10) 
+(check-expect (evalstr "((5+3)+(2*3))") 14)
+
+(test)
+
+
+
+
+
+                                        
