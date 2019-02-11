79448f69c1af02e5a1402312365a8b4ce57ff0c1
Connor <crjoyce2451@gmail.com>
2019-02-10 19:01:34 -0700

    Added valueless expressions, variable expressions, sequencing

--- Ni-parser.rkt
+++ Ni-parser.rkt
@@ -10,17 +10,20 @@
 
 (struct NameType(name kind next) #:transparent)
 (struct RecordType (name fields next) #:transparent)
-(struct ArrayType (name kind next) #:transparent)
-(struct TypeField (name kind) #:transparent)
+;;flipped kind and name because I put them in backwards, like everywhere. 
+(struct ArrayType (kind name next) #:transparent)
+(struct TypeField (kind name) #:transparent)
 
 (struct FunDecl (name args rettype body next) #:transparent)
 
 (struct NumExpr (val) #:transparent)
 (struct VarExpr (name) #:transparent)
 (struct Bool (val) #:transparent)
+;;
 (struct RecordExpr (name field) #:transparent)
 (struct ArrayExpr (name expr) #:transparent)
 (struct FuncallExpr (name args) #:transparent)
+;;
 (struct StringExpr (str) #:transparent)
 (struct NoVal () #:transparent)
 (struct LetExpr (decs exprs) #:transparent)
@@ -68,16 +71,17 @@
              (printf "Parsing error at line ~a, col ~a: token: ~a, value: ~a, tok-ok? ~a\n"
                      (lex:position-line start-pos) (lex:position-col start-pos) tok-name tok-value tok-ok?))))
    (grammar
-
     (expression
      ;pretty sure declarations aren't expressions but dont' know
      ;;what they would be if not expressions 
      [(NI type-id ID IS expression)                                                       (VarDecl $2 $3 $5)]
+     [(LPAREN RPAREN)                                                                     (NoVal)]
      [(NI ID IS expression)                                                               (VarDecl #f $2 $4)]
      [(IF expression THEN expression END)                                                 (IfExpr $2 $4 '())]
      [(IF expression THEN expression ELSE expression END)                                 (IfExpr $2 $4 $6)]
      [(NUM)                                                                               (NumExpr $1)]
      [(STRING)                                                                            (StringExpr $1)]
+     [(ID)                                                                                (VarExpr $1)]
      [(expression BOOLAND expression)                                                     (BoolExpr $1 #\& $3)]
      [(expression BOOLOR expression)                                                      (BoolExpr $1 #\| $3)]
      ;;Boolean literal struct needed?
@@ -87,7 +91,11 @@
      [(ID LBRACE assignment RBRACE)                                                       (NewRecordExpr $1 $3)]
         ;;pretty sure assignment causes shift/reduce conflict because it could be infinite recursion
      [(assignment)                                                                        $1]
+     [(LPAREN seq RPAREN)                                                                 $2]
      [(WHILE expression DO expression END)                                                (WhileExpr $2 $4)])
+    (seq
+     [(expression SEMI seq)                                                               (cons $1 $3)]
+     [(expression)                                                                        (cons $1 '())])
     (ty
      [(ID KIND AS type-id)                                                                (NameType $1 $4 '())]
      [(ID KIND AS LBRACE typefields RBRACE)                                               (RecordType $1 $5 '())]
