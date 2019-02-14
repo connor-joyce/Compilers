132d8c8e1106dcba8fa450e685e67b8bff6b9e17
connor-joyce <crjoyce2451@gmail.com>
2019-02-14 15:23:48 -0700

    updated types and typecheck in class. Typecheck has foundation for array types. Types still needs helper functions

--- typecheck.rkt
+++ typecheck.rkt
@@ -38,5 +38,21 @@
                              (let ([t1 (typecheck e1 env)]
                                    [t2 (typecheck e2 env)])
                                (cond [(and (types:IntType? t1) (types:IntType? t2))      (types:make-IntType)]))]
+           [(NewArrayExpr name num-elements init-val)     (let*
+                                                                ([arrty (types:actual-type (apply-env typeEnv name))]
+                                                                 [countty (types:actual-type (typecheck num-elements typeEnv))]
+                                                                 [initty (types:actual-type (typecheck init-val typeEnv))])
+                                                              (cond
+                                                                [(not (types:ArrayType? arrty))
+                                                                 ;;errors should be log-typeerror
+                                                                 (begin (error "~a must be an array type" ast name) (types:make-VoidType))]
+                                                                 [(not (types:IntType? countty))
+                                                                  (begin (error "number of elements in an array must be an int type" ast)
+                                                                         (types:make-VoidType))]
+                                                                 ;;types:type=? isn't defined yet, try to figure out this helper. 
+                                                                 [(not (types:type=? initty (types:ArrayType-element-type arrty)))
+                                                                  (begin (error "initialization value for array doesn't match type of array elements" ast)
+                                                                         (types:make-VoidType))])
+                                                                arrty)]
            [_                      (error "Type check error")])])
     type-of-expr))
