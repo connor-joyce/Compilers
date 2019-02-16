e7751c845eb0849a16dc89d9baa0151b74074d83
Connor <crjoyce2451@gmail.com>
2019-02-16 15:18:36 -0700

    Base cases working. VarDecs VarExpr ArrayExpr VoidTypes. Need to learn how to edit scopes to continue

---
+++ typecheck.rkt
@@ -0,0 +1,85 @@
+#lang racket
+(require (prefix-in types: "types.rkt")
+         "Ni-parser.rkt"
+         "environment.rkt")
+
+(provide (all-defined-out))
+
+;;initialize emtpy environment, then add base types
+(define (init-typeEnv)
+  (let ([tenv (empty-env)])
+    (extend-env tenv 'int (types:make-IntType))
+    (extend-env tenv 'bool (types:make-BoolType))
+    (extend-env tenv 'string (types:make-StringType))
+    (extend-env tenv 'x (types:make-IntType))
+    (extend-env tenv 'void (types:make-VoidType))
+    (extend-env tenv 'peng (types:make-PengType))))
+
+(define typeEnv (make-parameter (init-typeEnv)))
+
+(define (init-typechecker)
+  (typeEnv (init-typeEnv)))
+
+
+(define (tc-str str)
+  (init-typechecker)
+  ;;ast should be a list of structs. We want to typecheck them one by one
+  ;;my ast is just a struct rn
+  (let ([ast (parse-str str)])
+    (let ([ty (typecheck (first ast) (typeEnv))])
+          ty)))
+
+(define (typecheck ast env)
+  (let ([type-of-expr
+         (match ast  
+           [(NumExpr _)               (types:make-IntType)]
+           [(StringExpr _)            (types:make-StringType)]
+           [(BoolVal _)               (types:make-BoolType)]
+           [(PengExpr)                (types:make-PengType)]
+           [(VarDecl type name expr)    (let ([t1 (if (equal? type #f) #f (apply-env env (string->symbol type)))]
+                                              [t2 (typecheck expr env)])
+                                               (cond [(equal? t1 t2)   (types:make-VarValue t1)]
+                                                     [(and (not (equal? t2 types:PengType)) (equal? type #f))  (types:make-VarValue t2)]
+                                                     [else (error "Declared type: "t1" and given value type: "t2" must be the same\n")]))]
+           [(VarExpr name)                (apply-env env (string->symbol name))]
+           [(TypeField _ typ)             (typecheck typ env)]
+           [(ArrayExpr name index)   (let ([t1 (typecheck index env)]
+                                           [t2 (apply-env env (string->symbol name))])
+                                       (cond
+                                         [(types:IntType? t1)        t2]
+                                         [else                      (error "Index value must be of NumType")]))]
+           [(NoVal)                  (types:make-VoidType)]
+           [(Break)                  (types:make-VoidType)]
+           [(BoolExpr e1 op e2)      (let ([t1 (typecheck e1 env)]
+                                           [t2 (typecheck e2 env)])
+                                       (cond [(equal? t1 t2) (types:make-BoolType)]
+                                             [else (error "Both expressions in a boolean comparison must be the same type")]))]
+           [(MathExpr e1 op e2)
+                             (let ([t1 (typecheck e1 env)]
+                                   [t2 (typecheck e2 env)])
+                               (cond [(and (types:IntType? t1) (types:IntType? t2))      (types:make-IntType)]
+                                     [else (error "Both expressions need to be Number expressions in a math expression")]))]
+           [(LogicExpr e1 op e2) 
+                                       (let ([t1 (typecheck e1 env)]
+                                             [t2 (typecheck e2 env)])
+                                         (cond [(and (types:BoolType? t1) (types:BoolType? t2)) (types:make-BoolType)]
+                                               [else (error "Both expressions in a logic expression must be boolean expressions")]))]
+           
+           #;[(NewArrayExpr name num-elements init-val)     (let*
+                                                                ([arrty (types:actual-type (apply-env typeEnv name))]
+                                                                 [countty (types:actual-type (typecheck num-elements typeEnv))]
+                                                                 [initty (types:actual-type (typecheck init-val typeEnv))])
+                                                              (cond
+                                                                [(not (types:ArrayType? arrty))
+                                                                 ;;errors should be log-typeerror
+                                                                 (begin (error "~a must be an array type" ast name) (types:make-VoidType))]
+                                                                 [(not (types:IntType? countty))
+                                                                  (begin (error "number of elements in an array must be an int type" ast)
+                                                                         (types:make-VoidType))]
+                                                                 ;;types:type=? isn't defined yet, try to figure out this helper. 
+                                                                 [(not (types:type=? initty (types:ArrayType-element-type arrty)))
+                                                                  (begin (error "initialization value for array doesn't match type of array elements" ast)
+                                                                         (types:make-VoidType))])
+                                                                arrty)]
+           [_                      (error "Type check error")])])
+    type-of-expr))
