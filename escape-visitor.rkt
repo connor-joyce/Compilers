dced238960bff30a81cb1992f638b2d511bbd310
Connor <crjoyce2451@gmail.com>
2019-03-17 18:06:29 -0600

    just barebones on the escape visitor, not much here

--- escape-visitor.rkt
+++ escape-visitor.rkt
@@ -1,31 +1,35 @@
 #lang racket
 
 (require "environment.rkt"
-         "types.rkt"
+         ;"types.rkt"
          "Ni-parser.rkt")
 
 (provide (all-defined-out))
 
 (define escape-env (empty-env))
 
-(struct escape-info (escape? level))
+(struct escape-info (escape? level) #:transparent #:mutable)
 
 ;;public facing function
 (define (escape-visitor ast)
-  (escape-visitor-helper ast 0)) ;<-- public facing function calls backend helper which starts at level one
+  (escape-visitor-helper (first ast) 0)) ;<-- public facing function calls backend helper which starts at level one
 
 ;;assume this has passed through the typechecker
 ;;dont' check to see if variables are declared before usage
 (define (escape-visitor-helper ast level)
-  (match (first ast)
-    ;;all possible structs 
+  (match ast
+    ;;all possible structs
+    [(NumExpr _)          escape-env]
+    [(BoolVal _)          escape-env]
+    [(StringExpr _)       escape-env]
     [(BoolExpr e1 op e2)  (escape-visitor-helper e1 level)
                           (escape-visitor-helper e2 level)]
     [(MathExpr e1 op e2)  (escape-visitor-helper e1 level)
                           (escape-visitor-helper e2 level)]
     [(VarDecl _ id expr)  (extend-env escape-env id (escape-info #f level))
                           (escape-visitor-helper expr level)]
-    [(VarExpr name)       (let ([var (apply-env escape-env name)])
-                                (var-escape-info-escape? #true))]))
+    ;[(FuncDec _ args _ body next) (escape-visitor
+    [(VarExpr name)       (let ([var (apply-env escape-env (string->symbol name))])
+                            (set-escape-info-escape?! var #true))]))
                                 
   
